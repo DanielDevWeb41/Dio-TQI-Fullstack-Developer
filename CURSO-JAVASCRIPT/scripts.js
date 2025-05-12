56cee5064be62f6bb84d7fc3f21298332b164392
daniel <danielk15656@gmail.com>
2025-05-12 12:42:14 -0300

    Mais uma Aula feita

---
+++ CURSO-JAVASCRIPT/scripts.js
@@ -0,0 +1,13 @@
+//Tipos primitivos
+
+//boolean - valor booleano: verdadeiro ou falso
+var VouF = false;
+console.log(typeof VouF);
+
+//number - valor numerico
+var numeroQualquer = 1;
+console.log(typeof numeroQualquer)
+
+//string -"texto" 
+var nome = 'daniel'
+console;log(typeof nome);
